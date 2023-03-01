0f0818497badff033e3964a908db62e246de214a
Kevin <71006184+Kevin-ck1@users.noreply.github.com>
2023-03-02 00:26:53 +0300

    Second Commit

--- app.js
+++ app.js
@@ -9,35 +9,36 @@ class Book {
 
 //UI Class: User Interface
 class UI {
+    static StoredBooks = [
+        {
+            title: 'Book One',
+            author: 'John Doe',
+            isbn: '343434'
+        },
+        {
+            title: 'Book Two',
+            author: 'Jane Doe',
+            isbn: '1212123'
+        },
+    ];
     static displayBooks(){
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '343434'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '1212123'
-            },
-        ];
-
-        const books = StoredBooks;
+        const books = UI.StoredBooks;
 
         books.forEach((book)=>{
             UI.addBookToList(book);
         })
     }
 
+    
     static addBookToList(book){
         const list = document.querySelector('#book-list');
         const row = document.createElement('tr');
+
         row.innerHTML = `
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger btn-sm">x</a></td>
+            <td><a href="#" id="deleteButton" class="btn btn-danger btn-sm">x</a></td>
         `;
 
         list.appendChild(row);
@@ -49,6 +50,18 @@ class UI {
         document.querySelector('#author').value = '';
         document.querySelector('#isbn').value = '';
     }
+
+    //Delete Book
+    static deleteBook(event){
+        if(event.target.id === "deleteButton"){
+            const tr = event.target.closest('tr')
+            const tds = tr.querySelectorAll('td')
+            tr.style.display = "none"
+            const isbn = tds[2].innerText
+            const updatedBooks = UI.StoredBooks.filter(book => book.isbn !== isbn);
+        }
+    }
+
 }
 
 //Store Class: Handles Storage
@@ -73,10 +86,13 @@ document.querySelector('#book-form').addEventListener('submit',(event)=>{
 
     //Add book to UI
     UI.addBookToList(book);
+    UI.StoredBooks.push(book);
 
     //Clear Fields
     UI.clearFields();
 
 })
 //Event:Remove a book
-
+document.querySelector('tbody').addEventListener('click', (event)=>{
+    UI.deleteBook(event)
+})
